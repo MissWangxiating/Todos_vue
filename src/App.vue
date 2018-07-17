25052e5e5857d595b7e1420ee3276de4c3edaf97
wangxiating <1242682697@qq.com>
2018-07-17 22:07:45 +0800

    complete the vue program

--- src/App.vue
+++ src/App.vue
@@ -3,42 +3,55 @@
     <h1>{{title}}</h1>
     <input v-model="newItem" v-on:keyup.enter="addNew" type="text" name="mybox">
     <ul>
-      <li v-for="item in items" v-bind:class="{finished:item.isFinished}" v-on:click="togglefinish(item)">
+      <li v-for="item in items" :key="item.id" v-bind:class="{finished:item.isFinished}" v-on:click=" togglefinish(item) ">
         {{item.label}}
       </li>
     </ul>
+    <p>child tells me:{{childwords}}</p>
+    <component-a msgfromfather="you are going to die" v-on:child-tell-me-something='listenToMyBoy'></component-a>
   </div>
+
 </template>
 
 <script>
+import Store from "./store.js"
+import componentA from "./components/componentA"
 export default {
-  data: function(argument) {
+  data: function (argument) {
     // body...
-    return{
-      title:"this is a todo list",
-      items:[
-      {
-        label:"coding",
-        isFinished:false
-      },
-      {
-        label:"walking",
-        isFinished:true
-      }
-      ]
+    return {
+      title: 'this is a todo list',
+      items: Store.fetch(),
+      newItem: '',
+      childwords:''
+
     }
   },
-  methods:{
-    togglefinish: function(item){
-      //console.log(item)
-      item.isFinished = !item.isFinished;
+  components:{componentA}, //引入组件
+  watch: {
+    items:{
+      handler:function (items){
+        Store.save(items)
+      },
+      deep: true
+      }
     },
-    addNew:function(){
+
+  methods: {
+    togglefinish: function (item) {
+      // console.log(item)
+      item.isFinished = !item.isFinished
+    },
+    addNew: function () {
       this.items.push({
-        label:this.newItem,
-        isFinished:false
+        label: this.newItem,
+        isFinished: false
       })
-      this.newItem=" "
+      this.newItem = ''
+    },
+    listenToMyBoy: function(msg){
+      console.log(msg);
+      this.childwords = msg;
     }
   }
 }
---
+++ src/components/componentA.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{msgfromfather}}</h2>
+    <button v-on:click="onclickme">open mouse!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to componentA'
+    }
+  },
+  props:['msgfromfather'],
+  methods: {
+    onclickme: function ( ) {
+      // body...
+      console.log(this.msgfromfather);
+      this.$emit('child-tell-me-something',this.msg);
+    } 
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
